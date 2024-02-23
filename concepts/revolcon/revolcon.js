8f615776d774f4620a3611fae41a253223c74d49
cristianbyte <charlsmaritz@gmail.com>
2024-02-23 09:54:41 -0500

    updatedx2

--- concepts/revolcon/revolcon.js
+++ concepts/revolcon/revolcon.js
@@ -1,1 +1,10 @@
 const nav = document.querySelector('.nav__revolcon')
+const revolcon = document.querySelector('.revolcon')
+
+const repeat = 20;
+for(let i = 0; repeat==i ; i++){
+    const imageBack = document.createElement('div')
+    imageBack.classList.add('revolcon__image')
+    revolcon.appendChild(imageBack)
+    console.log(repeat);
+}
