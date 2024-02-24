44c37570c62439af19d05c95cdbd4b9dd4d233f0
Jeison <jeisonortiz1516@gmail.com>
2024-02-23 21:51:30 -0500

    create animation

--- concepts/topsyTurvyTumble/topsyTurvyTumble.js
+++ concepts/topsyTurvyTumble/topsyTurvyTumble.js
@@ -1,27 +1,29 @@
 const mainTopsy = document.querySelector(".topsyTurvy");
+const images = document.querySelectorAll(".imgToMove");
 
 document.addEventListener("DOMContentLoaded", () => {
-  renderImages();
+  // renderImages();
+  animatedImages();
 });
 
-mainTopsy.addEventListener("mousemove", (e) => {
-  const images = mainTopsy.querySelectorAll(".imgToMove");
-  images.forEach((image) => {
-    const xPosition = e.clientX;
-    const yPosition = e.clientY;
-    image.style.left = `${xPosition - image.width/2}px`;
-    image.style.top = `${yPosition - image.height/2}px`;
-  });
+function animatedImages() {
+  images.forEach(image=>{
+    image.addEventListener('mouseover', () => {
+      console.log("mouseover");
+      image.classList.add('animate__animated');
+      image.classList.add('animate__rotateOut');
+    });
+  
+    image.addEventListener('animationend', () => {
+      console.log("animationend");
 
-});
+      image.classList.remove('animate__animated');
+      image.classList.remove('animate__rotateOut');
+    });
+  })
+}
 
 /**render images of the database */
 function renderImages() {
-  const img = document.createElement("img");
-  img.classList.add("imgToMove");
-  img.src =
-    "https://gestion.pe/resizer/L3PoDXjbIFAMrbrDS7kzmhdUfRg=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/3QSLIQHSYFBHNEC5JP6YZEBMGU.jpg";
-  img.style.position = "absolute";
-  img.style.transition = "transform 0.2s ease-in-out";
-  mainTopsy.appendChild(img);
+  
 }
