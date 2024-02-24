a19e55aed3340ea2581c90d4f2165216aacac26a
lisSharik <lissharik@gmail.com>
2024-02-24 16:24:43 -0500

    Agregando controles de orbita

--- concepts/experimentalConcept/js/app.js
+++ concepts/experimentalConcept/js/app.js
@@ -1,6 +1,8 @@
 import * as THREE from "./threeJS/three.module.js";
+import {OrbitControls} from "./threeJS/orbitcontrols.js"
 
 const scene = new THREE.Scene();
+// scene.background = new THREE.Color(0x666666)
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -8,24 +10,54 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+  alpha: true,
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({
+
+//CUBE
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshStandardMaterial({
   color: 0x00ff00,
 });
 const cube = new THREE.Mesh(geometry, material);
+cube.castShadow = true;
+cube.position.set(1, 2, 1);
 scene.add(cube);
 
-camera.position.z = 5;
+//LIGHT
+const light = new THREE.DirectionalLight(0xffffff, 1, 100);
+light.position.set(-1, 1, 1);
+light.castShadow = true;
+scene.add(light);
 
-function animate() {
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+//PLANE
+const planeGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
+const planeMaterial = new THREE.MeshStandardMaterial({
+  color: 0xff0000,
+});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.receiveShadow = true;
+plane.position.set(0, 0, 0);
+scene.add(plane);
+
+camera.position.z = 5;
+camera.position.y = -3;
+camera.rotation.x = .5;
+
+const controls = new OrbitControls(camera, renderer.domElement)
+
+
+
+
+function render() {
+  // cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.01;
+  requestAnimationFrame(render);
   renderer.render(scene, camera);
 }
 
-animate();
+render();
