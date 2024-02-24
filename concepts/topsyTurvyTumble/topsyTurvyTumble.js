b4375cf9d34b75068ef3cde4ab1d6c38c9649465
Jeison <jeisonortiz1516@gmail.com>
2024-02-24 09:52:41 -0500

    functions to get, animate and render  images finished

--- concepts/topsyTurvyTumble/topsyTurvyTumble.js
+++ concepts/topsyTurvyTumble/topsyTurvyTumble.js
@@ -1,29 +1,68 @@
+//selectors
 const mainTopsy = document.querySelector(".topsyTurvy");
-const images = document.querySelectorAll(".imgToMove");
 
+//events
 document.addEventListener("DOMContentLoaded", () => {
-  // renderImages();
-  animatedImages();
+  getImages();
 });
 
-function animatedImages() {
-  images.forEach(image=>{
-    image.addEventListener('mouseover', () => {
-      console.log("mouseover");
-      image.classList.add('animate__animated');
-      image.classList.add('animate__rotateOut');
-    });
-  
-    image.addEventListener('animationend', () => {
-      console.log("animationend");
+//functions
 
-      image.classList.remove('animate__animated');
-      image.classList.remove('animate__rotateOut');
+/**function to animate the images */
+function animatedImages() {
+  const images = document.querySelectorAll(".imgToMove");
+  images.forEach((image) => {
+    image.addEventListener("mouseover", () => {
+      image.classList.add("animate__animated");
+      image.classList.add("animate__rotateOut");
     });
-  })
+
+    image.addEventListener("animationend", () => {
+      image.classList.remove("animate__animated");
+      image.classList.remove("animate__rotateOut");
+    });
+  });
 }
 
-/**render images of the database */
-function renderImages() {
-  
+/**get images of the database */
+async function getImages() {
+  const url = `http://localhost:3000/images`;
+  const response = await fetch(url);
+  const data = await response.json();
+  const images = fillImages(data);
+  const randomImages = reOrderImages(images);
+  renderImages(randomImages);
+}
+
+/**This function reorder images.
+ * if negative: image before next image,
+ * if positive: image after next image,
+ * if zero: image after next image,
+ */
+function reOrderImages(data) {
+  return data.sort(() => Math.random() - 0.5);
+
+}
+
+/**render images*/
+function renderImages(images) {
+  images.forEach((image) => {
+    const img = document.createElement("img");
+    img.src = image.url;
+    img.classList.add("imgToMove");
+    mainTopsy.appendChild(img);
+  });
+  animatedImages();
+}
+
+/**fill an array with at least 28 elements */
+function fillImages(data) {
+  let images = [];
+  while (images.length < 28) {
+    data.forEach((image) => images.push(image));
+
+  }
+  //delete images beyond 28
+  images = images.slice(0,28);
+  return images;
 }
