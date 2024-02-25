273dbc097b4b34e0d34da2e0f119e463bee69505
cristianbyte <charlsmaritz@gmail.com>
2024-02-25 10:48:15 -0500

    updated(home)

--- src/script.js
+++ src/script.js
@@ -9,11 +9,15 @@ async function loadPages(){
 (async () => {
     const pages = await loadPages();
     pages.forEach((page) => {
+        const redirect = document.createElement('a')
         const iframe = document.createElement('iframe')
+        redirect.href = page.url
+        redirect.classList.add('overlay')
+        redirect.target = '_blanck'
         iframe.src = page.url
-        app.appendChild(iframe)
+        redirect.appendChild(iframe)
+        app.appendChild(redirect)
         let name = page.name
         let link = page.url
-        console.log(name,link);
     })
 })();
