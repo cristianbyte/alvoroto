45f4ab8fbee4a7ebba6d52e6837aedfe6ba9d571
sararuiza <149072353+sararuiza@users.noreply.github.com>
2024-02-26 20:36:35 -0500

    conceptoSara-puntuacion bolos js

--- concepts/conceptoSara/app.js
+++ concepts/conceptoSara/app.js
@@ -1,7 +1,42 @@
 //selectores
-const tbody = document.querySelector("#tbody");
+
+const table = document.querySelector(".table")
+const thead= document.createElement("thead")
+const tbody = document.createElement("tbody")
 
 
-tbody.addEventListener("click",()=>{
-    console.log("entrando");
+
+window.addEventListener("DOMContentLoaded", ()=>{
+   createThead() 
+   table.appendChild(tbody)
+   createContentTable() 
 })
+
+
+function createThead(){
+    const tr= document.createElement("tr")
+    thead.appendChild(tr)
+
+    for (let index = 0; index < 12; index++) {
+        const th = document.createElement("th")
+        th.textContent= "Round " +index  
+        tr.appendChild(th)
+    }
+
+    tr.firstChild.textContent = "Player"
+    tr.lastChild.textContent = "Total points"
+   table.appendChild(thead)
+
+}
+
+function createContentTable(){
+    const tr= document.createElement("tr")
+    tbody.appendChild(tr)
+
+    for (let index = 0; index < 12; index++) {
+        const td = document.createElement("td")
+        td.setAttribute("contenteditable", "true")
+        tr.appendChild(td)
+    }
+    
+}
