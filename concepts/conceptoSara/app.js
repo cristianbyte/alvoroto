880844a3b9af472308aba9c26b0d7c1c11b1e2bc
sararuiza <149072353+sararuiza@users.noreply.github.com>
2024-02-27 14:06:23 -0500

    suma de puntos

--- concepts/conceptoSara/app.js
+++ concepts/conceptoSara/app.js
@@ -1,42 +1,61 @@
 //selectores
 
-const table = document.querySelector(".table")
-const thead= document.createElement("thead")
-const tbody = document.createElement("tbody")
+const table = document.querySelector(".table");
+const thead = document.createElement("thead");
+const tbody = document.createElement("tbody");
 
+window.addEventListener("DOMContentLoaded", () => {
+  createThead();
+  table.appendChild(tbody);
+  createContentTable();
+});
 
+function createThead() {
+  const tr = document.createElement("tr");
+  thead.appendChild(tr);
 
-window.addEventListener("DOMContentLoaded", ()=>{
-   createThead() 
-   table.appendChild(tbody)
-   createContentTable() 
-})
+  for (let index = 0; index < 12; index++) {
+    const th = document.createElement("th");
+    th.textContent = "Round " + index;
+    tr.appendChild(th);
+  }
 
-
-function createThead(){
-    const tr= document.createElement("tr")
-    thead.appendChild(tr)
-
-    for (let index = 0; index < 12; index++) {
-        const th = document.createElement("th")
-        th.textContent= "Round " +index  
-        tr.appendChild(th)
-    }
-
-    tr.firstChild.textContent = "Player"
-    tr.lastChild.textContent = "Total points"
-   table.appendChild(thead)
-
+  tr.firstChild.textContent = "Player";
+  tr.lastChild.textContent = "Total points";
+  table.appendChild(thead);
 }
 
-function createContentTable(){
-    const tr= document.createElement("tr")
-    tbody.appendChild(tr)
+function createContentTable() {
+  const tr = document.createElement("tr");
 
-    for (let index = 0; index < 12; index++) {
-        const td = document.createElement("td")
-        td.setAttribute("contenteditable", "true")
-        tr.appendChild(td)
+  tbody.appendChild(tr);
+
+  for (let index = 0; index < 12; index++) {
+    const td = document.createElement("td");
+    if (index == 0) {
+      td.textContent = "Player";
+      tr.appendChild(td);
+      continue;
     }
-    
+    const inputNumber = document.createElement("input");
+    inputNumber.classList.add("inputNumber");
+    inputNumber.setAttribute("type", "number");
+    td.appendChild(inputNumber);
+    td.classList.add("tdTable");
+
+    inputNumber.addEventListener("input", () => {
+      tr.lastChild.textContent= sumPoints(tr);
+    });
+    tr.appendChild(td);
+  }
+
+  const tdTable = document.querySelector(".tdTable");
+}
+
+function sumPoints(tr) {
+  let result = 0;
+  for (let index = 1; index <= 10; index++) {
+    result += Number(tr.childNodes[index].firstChild.value);
+  }
+  return result;
 }
